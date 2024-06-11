0d3ba39eb9f1181f0d3ff3e71fabfc7d2ea66683
GusEngers <gustavoengers6441@gmail.com>
2024-06-10 23:40:40 -0300

    Calculator component & Data buttons

---
+++ src/Calculator/data.js
@@ -0,0 +1,70 @@
+export const DATA_BUTTONS = [
+  {
+    value: '=',
+    name: 'equals',
+  },
+  {
+    value: '0',
+    name: 'zero',
+  },
+  {
+    value: '1',
+    name: 'one',
+  },
+  {
+    value: '2',
+    name: 'two',
+  },
+  {
+    value: '3',
+    name: 'three',
+  },
+  {
+    value: '4',
+    name: 'four',
+  },
+  {
+    value: '5',
+    name: 'five',
+  },
+  {
+    value: '6',
+    name: 'six',
+  },
+  {
+    value: '7',
+    name: 'seven',
+  },
+  {
+    value: '8',
+    name: 'eight',
+  },
+  {
+    value: '9',
+    name: 'nine',
+  },
+  {
+    value: '+',
+    name: 'add',
+  },
+  {
+    value: '-',
+    name: 'subtract',
+  },
+  {
+    value: '*',
+    name: 'multiply',
+  },
+  {
+    value: '/',
+    name: 'divide',
+  },
+  {
+    value: '.',
+    name: 'decimal',
+  },
+  {
+    value: 'AC',
+    name: 'clear',
+  },
+];
